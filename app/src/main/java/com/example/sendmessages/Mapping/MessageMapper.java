ceaa05329d6bf6aca5dafd06ae9d45e218a3c1c7
kirlozavr <kirill.protchenko@mail.ru>
2022-12-27 00:01:51 +1000

    Была добавлена проверка даты с сегодняшней и вывод соответствующий Dto

--- app/src/main/java/com/example/sendmessages/Mapping/MessageMapper.java
+++ app/src/main/java/com/example/sendmessages/Mapping/MessageMapper.java
@@ -11,6 +11,8 @@ import java.time.ZonedDateTime;
 
 public class MessageMapper implements Mapping<MessageEntity, MessageDto> {
 
+    private boolean isToday;
+
     @Override
     public MessageDto getEntityToDto(@NonNull MessageEntity entity) {
         ZonedDateTime zonedDateTime = ZonedDateTime.parse(
@@ -18,15 +20,27 @@ public class MessageMapper implements Mapping<MessageEntity, MessageDto> {
                 DateFormat.getFormatFromDataBase()
                 );
 
-        return new MessageDto(
-                entity.getMessage(),
-                DateFormat.getFormatToMessages().format(zonedDateTime),
-                entity.getUsernameFrom()
-        );
+        if(isToday) {
+            return new MessageDto(
+                    entity.getMessage(),
+                    DateFormat.getFormatToDateAndTime().format(zonedDateTime),
+                    entity.getUsernameFrom()
+            );
+        } else {
+            return new MessageDto(
+                    entity.getMessage(),
+                    DateFormat.getFormatToTime().format(zonedDateTime),
+                    entity.getUsernameFrom()
+            );
+        }
     }
 
     @Override
     public MessageEntity getDtoToEntity(MessageDto dto) {
         return null;
+    }
+
+    public void setIsToday(boolean isToday){
+        this.isToday = isToday;
     }
 }
