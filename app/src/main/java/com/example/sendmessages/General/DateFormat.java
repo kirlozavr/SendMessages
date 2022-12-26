c0133195dd138355489ed5b086d11d1de8f99770
kirlozavr <kirill.protchenko@mail.ru>
2022-12-26 11:47:33 +1000

    В формат была добавленна Locale

--- app/src/main/java/com/example/sendmessages/General/DateFormat.java
+++ app/src/main/java/com/example/sendmessages/General/DateFormat.java
@@ -1,21 +1,22 @@
 package com.example.sendmessages.General;
 
 import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 public class DateFormat {
 
     public static DateTimeFormatter getFormatToDataBase(){
         return DateTimeFormatter
-                .ofPattern("dd-MM-yyyy HH:mm:ss:SSSS OOOO");
+                .ofPattern("dd-MM-yyyy HH:mm:ss.SSS OOOO", Locale.ENGLISH);
     }
 
     public static DateTimeFormatter getFormatToMessages(){
         return DateTimeFormatter
-                .ofPattern("dd-MM-yyyy HH:mm");
+                .ofPattern("dd-MM-yyyy HH:mm", Locale.ENGLISH);
     }
 
     public static DateTimeFormatter getFormatFromDataBase(){
         return DateTimeFormatter
-                .ofPattern("dd-MM-yyyy HH:mm:ss:SSSS zzzz");
+                .ofPattern("dd-MM-yyyy HH:mm:ss.SSS zzzz", Locale.ENGLISH);
     }
 }
