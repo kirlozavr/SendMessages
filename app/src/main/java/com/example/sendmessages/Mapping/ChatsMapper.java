ba54f41aff2654072bc006d22b65d46730deaac2
kirlozavr <kirill.protchenko@mail.ru>
2022-12-26 13:51:03 +1000

    В mapper добавлена конвертация времени из базы данных

--- app/src/main/java/com/example/sendmessages/Mapping/ChatsMapper.java
+++ app/src/main/java/com/example/sendmessages/Mapping/ChatsMapper.java
@@ -1,16 +1,27 @@
 package com.example.sendmessages.Mapping;
 
+import androidx.annotation.NonNull;
+
 import com.example.sendmessages.DTO.ChatsDto;
 import com.example.sendmessages.Entity.ChatsEntity;
+import com.example.sendmessages.General.DateFormat;
 import com.example.sendmessages.Interface.Mapping;
+
+import java.time.ZonedDateTime;
 
 public class ChatsMapper implements Mapping<ChatsEntity, ChatsDto> {
 
     @Override
-    public ChatsDto getEntityToDto(ChatsEntity entity) {
+    public ChatsDto getEntityToDto(@NonNull ChatsEntity entity) {
+        ZonedDateTime zonedDateTime = ZonedDateTime.parse(
+                entity.getTimeMessageToDataBase(),
+                DateFormat.getFormatFromDataBase()
+        );
+
         return new ChatsDto(
                 entity.getUsernameToWhom(),
-                entity.getLastMessage()
+                entity.getLastMessage(),
+                DateFormat.getFormatToMessages().format(zonedDateTime)
         );
     }
 
