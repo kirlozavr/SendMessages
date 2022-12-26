137c31abd48b0b1a8864b08744b16ff7ea6c0757
kirlozavr <kirill.protchenko@mail.ru>
2022-12-27 00:00:34 +1000

    Была добавлена проверка даты с сегодняшней и вывод соответствующий Dto

--- app/src/main/java/com/example/sendmessages/Mapping/ChatsMapper.java
+++ app/src/main/java/com/example/sendmessages/Mapping/ChatsMapper.java
@@ -11,22 +11,35 @@ import java.time.ZonedDateTime;
 
 public class ChatsMapper implements Mapping<ChatsEntity, ChatsDto> {
 
+    private boolean isToday;
+
     @Override
     public ChatsDto getEntityToDto(@NonNull ChatsEntity entity) {
         ZonedDateTime zonedDateTime = ZonedDateTime.parse(
                 entity.getTimeMessageToDataBase(),
                 DateFormat.getFormatFromDataBase()
         );
-
-        return new ChatsDto(
-                entity.getUsernameToWhom(),
-                entity.getLastMessage(),
-                DateFormat.getFormatToMessages().format(zonedDateTime)
-        );
+        if(isToday) {
+            return new ChatsDto(
+                    entity.getUsernameToWhom(),
+                    entity.getLastMessage(),
+                    DateFormat.getFormatToDateAndTime().format(zonedDateTime)
+            );
+        } else {
+            return new ChatsDto(
+                    entity.getUsernameToWhom(),
+                    entity.getLastMessage(),
+                    DateFormat.getFormatToTime().format(zonedDateTime)
+            );
+        }
     }
 
     @Override
     public ChatsEntity getDtoToEntity(ChatsDto dto) {
         return null;
+    }
+
+    public void setIsToday(boolean isToday){
+        this.isToday = isToday;
     }
 }
