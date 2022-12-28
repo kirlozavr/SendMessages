ec274636eabcc32d3b06d2f5c0498bc55a7f9faa
kirlozavr <kirill.protchenko@mail.ru>
2022-12-28 13:49:20 +1000

    Константа была перемещена в класс Data

--- app/src/main/java/com/example/sendmessages/General/DataBase.java
+++ app/src/main/java/com/example/sendmessages/General/DataBase.java
@@ -5,10 +5,8 @@ public class DataBase {
     public static final String MESSAGES_DB = "Messages";
     public static final String CHATS_DB = "Chats";
 
-    public class SettingsTag{
-        public static final String SETTINGS_TAG = "mySettings";
+    public class ListTag {
         public static final String COLLECTIONS_CHATS_TAG = "ListChats";
         public static final String COLLECTIONS_MESSAGES_TAG = "ListMessages";
-        public static final String USER_NAME_TAG = "username";
     }
 }
