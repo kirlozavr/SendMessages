5638be379dfc189ba68d2a42ce201bcae9b6b724
kirlozavr <kirill.protchenko184@gmail.com>
2024-07-03 16:58:14 +1000

    changed

--- app/src/main/java/com/example/sendmessages/Entity/UserEntity.kt
+++ app/src/main/java/com/example/sendmessages/Entity/UserEntity.kt
@@ -1,7 +1,7 @@
 package com.example.sendmessages.Entity
 
 data class UserEntity(
-    val userId: Long,
-    val username: String,
-    val password: String
+    val userId: Long = 0,
+    val username: String = "",
+    val password: String = ""
 )
